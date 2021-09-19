3746061ef023ae82998dcc6d79d441fd47cfc45b
ilicina1 <ilhan.licina@outlook.com>
2021-09-19 22:39:29 +0200

    s

---
+++ android/app/src/main/kotlin/com/anoop/fluttermqttnew/MainActivity 2.kt
@@ -0,0 +1,6 @@
+package com.mqtt_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/core/widgets/status_bar 2.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class StatusBar extends StatelessWidget {
+  String _statusMessage;
+  StatusBar({required statusMessage}) : _statusMessage = statusMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildConnectionStateText(_statusMessage);
+  }
+
+  Widget _buildConnectionStateText(String status) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+              color: Color(0xff01579B),
+              child: Text(
+                status,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Poppins",
+                ),
+              )),
+        ),
+      ],
+    );
+  }
+}
